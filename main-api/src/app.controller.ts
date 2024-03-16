2ad8c25d72d8c91d7596b04db897e48fa546b75e
hara.junya <hara.junya@plus-zero.co.jp>
2024-03-16 08:35:58 +0000

    feat: test prisma

--- main-api/src/app.controller.ts
+++ main-api/src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly appService: AppService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/all-results')
+  async getAllResults(): Promise<any> {
+    return await this.prisma.aiAnalysisLog.findMany();
   }
 }
